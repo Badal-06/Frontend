82a0fe541bdbb57cb52ca3868ece0e36ef56ff0e
badal-06 <badalpanwar91@gmail.com>
2026-01-22 16:03:48 +0530

    Addin JavaScript Folder

---
+++ JavaScript/Guess_Game.js
@@ -0,0 +1,9 @@
+let gameNum=25;
+
+let userNum= prompt("Guess a number ");
+
+while(userNum!=gameNum){
+    userNum=prompt("Your Guess is wrong try again :- ")
+}
+
+console.log("Congratulation , You Guessed the right number")
